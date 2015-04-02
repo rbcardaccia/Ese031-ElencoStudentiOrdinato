ae9e456884f91b4eb567ad6da32f72667d5dde28
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-04-02 13:31:41 +0200

    Da completare

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definisci le costanti
+
+// Definisci il tipo di dato struct s_studente
+
+// Definisci studente come struct s_studente
+
+// Dichiara il prototipo della funzione di ordinamento
+
+
+int main(int argc, char** argv) {
+    // Dichiara le variabili locali
+    
+    // Codice per l'inserimento, da parte dell'utente, dei dati
+    
+    // Ordina l'elenco (con funzione)
+    
+    // Codice per la stampa dei dati (uno studente per riga)
+    
+    return (EXIT_SUCCESS);
+}
+
+// Definisci la funzione di ordinamento
